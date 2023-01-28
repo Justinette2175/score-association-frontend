8d661fb310e278f00cd9e74441dad1d5a07ef1da
justinette2175 <jugagnepain75@gmail.com>
2023-01-28 11:38:00 -0500

    update axios

--- frontend/src/Services/SubscribersService.ts
+++ frontend/src/Services/SubscribersService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { CreateSubscriberParameters } from "../Types";
 
-export const AxiosInstance = axios.create({
+export const AxiosSubscribeInstance = axios.create({
   baseURL: process.env.SCORE_ASSOCIATION_SUBSCRIBE_AWS_GATEWAY_URL,
   timeout: 1000
 });
@@ -12,7 +12,7 @@ export async function createSubscriber({
   email,
   name
 }: CreateSubscriberParameters) {
-  return AxiosInstance.post(SUBSCRIBERS_ROUTE, {
+  return AxiosSubscribeInstance.post(SUBSCRIBERS_ROUTE, {
     data: {
       email,
       name
