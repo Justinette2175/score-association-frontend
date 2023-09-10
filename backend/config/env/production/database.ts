79bb68a3c9937dc562b96285ac0e46148a19e291
justinette2175 <jugagnepain75@gmail.com>
2023-09-10 16:27:04 -0400

    update config for db

--- backend/config/env/production/database.ts
+++ backend/config/env/production/database.ts
@@ -1,20 +1,13 @@
-import { parse } from "pg-connection-string";
-
-const config = parse(process.env.DATABASE_URL);
-
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
