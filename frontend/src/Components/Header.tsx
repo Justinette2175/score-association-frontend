428b3e103ac282532a6500dd5eb49ed1009ee5db
justinette2175 <jugagnepain75@gmail.com>
2023-09-19 10:38:27 -0400

    remove unused variables

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FRONT_END_ROUTES } from "../Routes";
-import { Menu } from "./Menu";
 
 export function Header(): React.ReactElement {
   return (
